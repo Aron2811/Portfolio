eafae999513b0d37e31f8e8ad38c3a2a105681f1
John Aron <marasiganjohnaron1@gmail.com>
2025-11-06 18:23:23 +0800

    Added hover zoom effect to project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
         <a href="https://todolistwithfirestore.vercel.app/" 
            target="_blank" 
           rel="noopener noreferrer">
-        <img  src={projectOne} alt="" className='relative rounded-2xl hover:opacity-80 transition duration-300 cursor-pointer'  />
+        <img  src={projectOne} alt="" className='relative rounded-2xl  hover:opacity-80 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer'  />
         </a>
         <h2 className='flex justify-center text-2xl tracking-wide font-bold '>Todolist</h2>
         <p className='leading-relaxed  text-justify '>A to-do list website designed for desktop (not for mobile) with added features to help users manage tasks. Users can mark tasks as Work or Personal, set due dates, and view upcoming tasks in the “Upcoming” tab. Tasks due within 5 days are shown with a priority tag, and each task shows how many days are left. The website is built using React.js and Tailwind CSS, uses Firestore as a NoSQL database to save tasks, and allows users to log in with Google.</p>
@@ -36,7 +36,7 @@ const Projects = () => {
         <a href="https://payrestroomtracker.github.io/PayrestroomTracker/?fbclid=IwY2xjawN1ejpleHRuA2FlbQIxMABicmlkETFrdkdTc3hzbWpuWDVyRDFPAR6y7zFobFqhAsVxkaDcTN6A_NjMl1mJI9wgb7OFJqJJHwhcRf3oFhb8Rn2TyQ_aem_W22PaXAmaNB9Ns5AbSE1hA" 
            target="_blank" 
           rel="noopener noreferrer">
-        <img src={Thesis} alt="" className='rounded-2xl w-35 xl:w-50 hover:opacity-80 transition duration-300 cursor-pointer' />
+        <img src={Thesis} alt="" className='rounded-2xl w-35 xl:w-50 hover:opacity-80 transition duration-300  ease-in-out transform hover:scale-105 cursor-pointer' />
         </a>
         <h2 className='flex justify-center text-2xl tracking-wide font-bold '>Thesis</h2>
         <p className='leading-relaxed text-justify '>Our thesis, "A Mobile-Based Paid Restroom Tracker Using A* Pathfinding Algorithm," is an application that finds the nearest paid restroom. My responsibilities included setting up and managing the Firebase Firestore database for storing and retrieving app data, handling backend functionalities such as saving, updating, and fetching data, developing frontend UI components in Flutter connected to the database for real-time updates, applying security measures such as muting or banning accounts to prevent misuse, conducting testing and debugging to ensure reliability, and collaborating closely with groupmates to integrate features across the system.</p>
@@ -46,14 +46,14 @@ const Projects = () => {
           <a href="https://katseye-inky.vercel.app/" 
            target="_blank" 
           rel="noopener noreferrer">
-        <img src={projectTwo} alt="" className='relative rounded-2xl hover:opacity-80 transition duration-300 cursor-pointer' />
+        <img src={projectTwo} alt="" className='relative rounded-2xl hover:opacity-80 transition ease-in-out transform hover:scale-105 duration-300 cursor-pointer' />
         </a>
         <h2 className='flex justify-center text-2xl tracking-wide font-bold '>KATSEYE PROFILE</h2>
         <p className='leading-relaxed  text-justify lg:justify-center xl:justify-center flex'>A Katseye profile website dedicated to Katseye, built using React.js and Tailwind CSS.</p>
         </div>
 
         <div data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1000" className='flex flex-col gap-5 '>
-        <img src={Portfolio} alt="" className='relative rounded-2xl border border-white' />
+        <img src={Portfolio} alt="" className='relative rounded-2xl border border-white ' />
         <h2 className='flex justify-center text-2xl tracking-wide font-bold '>My Portfolio</h2>
         <p className='leading-relaxed  text-justify lg:justify-center xl:justify-center md:justify-center flex '>My portfolio, built using React.js and Tailwind CSS.</p>
         </div>
